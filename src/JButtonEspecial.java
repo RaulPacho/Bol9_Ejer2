6020d5e660e18175610b849c05ec2172e81ecec8
RaulPacho <64408143+RaulPacho@users.noreply.github.com>
2020-05-12 19:03:15 +0200

    Add files via upload

---
+++ src/JButtonEspecial.java
@@ -0,0 +1,10 @@
+import javax.swing.JButton;
+
+public class JButtonEspecial extends JButton {
+    public boolean clickeao;
+
+    public JButtonEspecial(String e){
+        super(e);
+        clickeao=false;
+    }
+}
